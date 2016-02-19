4bdd1946e33753c3ac353f27b2003d0c8bcf8741
Cody Stoltman <particlebanana@gmail.com>
2016-02-19 14:29:59 -0600

    add tests for LIKE

---
+++ test/builder/builder.like.test.js
@@ -0,0 +1,160 @@
+var Builder = require('../../index').builder;
+var analyze = require('../support/analyze');
+var assert = require('assert');
+
+describe('Builder ::', function() {
+  describe('LIKE operator', function() {
+    it('should generate a LIKE query', function(done) {
+      var tree = analyze({
+        select: '*',
+        from: 'users',
+        where: {
+          or: [
+            {
+              name: {
+                like: '%Test%'
+              }
+            },
+            {
+              not: {
+                id: {
+                  in: [1, 2, 3]
+                }
+              }
+            }
+          ]
+        }
+      });
+
+      Builder({
+        tree: tree
+      })
+      .exec(function(err, result) {
+        assert(!err);
+        assert.deepEqual(result, {
+          find: 'users',
+          filter: {
+            '$or': [
+              {
+                name: /Test/
+              },
+              {
+                id: {
+                  '$nin': [1, 2, 3]
+                }
+              }
+            ]
+          },
+          sort: {},
+          projection: {},
+          skip: 0,
+          limit: 0
+        });
+
+        return done();
+      });
+    });
+
+    it('should generate a startsWith query', function(done) {
+      var tree = analyze({
+        select: '*',
+        from: 'users',
+        where: {
+          or: [
+            {
+              name: {
+                like: 'Test%'
+              }
+            },
+            {
+              not: {
+                id: {
+                  in: [1, 2, 3]
+                }
+              }
+            }
+          ]
+        }
+      });
+
+      Builder({
+        tree: tree
+      })
+      .exec(function(err, result) {
+        assert(!err);
+        assert.deepEqual(result, {
+          find: 'users',
+          filter: {
+            '$or': [
+              {
+                name: /^Test/
+              },
+              {
+                id: {
+                  '$nin': [1, 2, 3]
+                }
+              }
+            ]
+          },
+          sort: {},
+          projection: {},
+          skip: 0,
+          limit: 0
+        });
+
+        return done();
+      });
+    });
+
+    it('should generate an endsWith query', function(done) {
+      var tree = analyze({
+        select: '*',
+        from: 'users',
+        where: {
+          or: [
+            {
+              name: {
+                like: '%Test'
+              }
+            },
+            {
+              not: {
+                id: {
+                  in: [1, 2, 3]
+                }
+              }
+            }
+          ]
+        }
+      });
+
+      Builder({
+        tree: tree
+      })
+      .exec(function(err, result) {
+        assert(!err);
+        assert.deepEqual(result, {
+          find: 'users',
+          filter: {
+            '$or': [
+              {
+                name: /Test$/
+              },
+              {
+                id: {
+                  '$nin': [1, 2, 3]
+                }
+              }
+            ]
+          },
+          sort: {},
+          projection: {},
+          skip: 0,
+          limit: 0
+        });
+
+        return done();
+      });
+    });
+  });
+});
